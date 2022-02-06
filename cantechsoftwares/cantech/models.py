b263b5e2eca2cf84b29792f953882f3b9cac9c36
Ilmaan <ilmaacc@gmail.com>
2022-02-07 00:35:12 +0530

    Contact us and model initiation

--- cantechsoftwares/cantech/models.py
+++ cantechsoftwares/cantech/models.py
@@ -8,3 +8,8 @@ class FeedbackModel(models.Model):
     f_phoneno = models.CharField(max_length=12,blank=True)
     f_email = models.EmailField()
     f_message = models.TextField(max_length=500)
+
+
+class DevelopersModel(models.Model):
+    d_name = models.CharField(max_length=30,blank=True)
+    
